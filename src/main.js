201979b2b21678169fc065e5f0ac630ae00f6e3f
gt3060 <1017194476@qq.com>
2020-08-13 17:47:40 +0800

    feat 增加vue引用iconfont中官方图表功能，

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui';
+import './assets/icon/iconfont.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
