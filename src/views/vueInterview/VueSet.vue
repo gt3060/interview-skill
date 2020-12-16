b80c79673689c7962450d09a0df6cfe902075de8
gt3060 <1017194476@qq.com>
2020-12-16 14:21:21 +0800

    feat 增加component标签内容，封装高亮代码模块方法

--- src/views/vueInterview/VueSet.vue
+++ src/views/vueInterview/VueSet.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="vueDirective">
-    <div class="fontStyle">
-      <p class="fontSize">1.什么是Vue.set?</p>
-      <p class="fontIndent">
-        Vue.set 的作用就是在构造器外部操作构造器内部的数据、属性或者方法。
-      </p>
-    </div>
-    <div class="fontStyle">
-      <p class="fontSize">2.引用构造器外部数据</p>
-      <p class="fontIndent">引用构造器外部数据count：{{ count }}，代码如下：</p>
-    </div>
-    <div class="codeBorder fontCodeStyle">
-      <pre class="codeBorder">
+    <div class="vueDirective">
+        <div class="fontStyle">
+            <p class="fontSize">1.什么是Vue.set?</p>
+            <p class="fontIndent">
+                Vue.set 的作用就是在构造器外部操作构造器内部的数据、属性或者方法。
+            </p>
+        </div>
+        <div class="fontStyle">
+            <p class="fontSize">2.引用构造器外部数据</p>
+            <p class="fontIndent">引用构造器外部数据count：{{ count }}，代码如下：</p>
+        </div>
+        <div class="codeBorder fontCodeStyle">
+            <pre class="codeBorder">
         &lt;script&gt;<br/>
         let outData = {<br/>
           count: 1,<br/>
@@ -24,47 +24,50 @@
         &lt;/script&gt;<br/>
         &lt;p&gt;{ { count } } &lt;/p&gt;
       </pre>
-    </div>
-    <div class="fontStyle">
-      <p class="fontSize">3.改变构造器外部数据</p>
-      <p class="fontIndent">
-        每次点击按钮，构造器外部数据count加1：{{ count }} &nbsp; &nbsp; &nbsp;
-        &nbsp; &nbsp;
-        <el-button type="text" @click="changeCount">添加</el-button>
-        代码如下：
-      </p>
-    </div>
-    <div class="codeBorder fontCodeStyle">
-      <pre class="codeBorder">
+        </div>
+        <div class="fontStyle">
+            <p class="fontSize">3.改变构造器外部数据</p>
+            <p class="fontIndent">
+                每次点击按钮，构造器外部数据count加1：{{ count }} &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp;
+                <el-button type="text"
+                           @click="changeCount">添加</el-button>
+                代码如下：
+            </p>
+        </div>
+        <div class="codeBorder fontCodeStyle">
+            <pre class="codeBorder">
         // 方法一：利用Vue.set <br/>
         Vue.set(outData, "count", this.count + 1); <br/>
         // 方法二：利用this <br/>
         // this.count++; <br/>
         // 方法三： 直接操作外部数据(原生方法) <br/>
-        // outData.count++; 
+        // outData.count++;
       </pre>
-    </div>
-    <div class="fontStyle">
-      <p class="fontSize">4.为什么采用Vue.set</p>
-      <p class="fontIndent">
-        由于Javascript的限制，Vue不能自动检测以下变动的数组。
-      </p>
-      <div class="labelMarginLeft">
-        <ol type="a">
-          <li>当你利用索引直接设置一个项时，vue不会为我们自动更新。</li>
-          <li>当你修改数组长度时，vue不会自动更新。</li>
-        </ol>
-        <p>例如:</p>
-        <ul>
-          <li v-for="(item, index) in arr" :key="index">{{ item }}</li>
-        </ul>
-        <el-button @click="changeArr" type="text">changeArr</el-button>
-        <p>问题：点击按钮发现视图并没有变，解决办法就是利用Vue.set</p>
-        <p>代码如下：</p>
-      </div>
-    </div>
-    <div class="codeBorder fontCodeStyle">
-      <pre class="codeBorder">
+        </div>
+        <div class="fontStyle">
+            <p class="fontSize">4.为什么采用Vue.set</p>
+            <p class="fontIndent">
+                由于Javascript的限制，Vue不能自动检测以下变动的数组。
+            </p>
+            <div class="labelMarginLeft">
+                <ol type="a">
+                    <li>当你利用索引直接设置一个项时，vue不会为我们自动更新。</li>
+                    <li>当你修改数组长度时，vue不会自动更新。</li>
+                </ol>
+                <p>例如:</p>
+                <ul>
+                    <li v-for="(item, index) in arr"
+                        :key="index">{{ item }}</li>
+                </ul>
+                <el-button @click="changeArr"
+                           type="text">changeArr</el-button>
+                <p>问题：点击按钮发现视图并没有变，解决办法就是利用Vue.set</p>
+                <p>代码如下：</p>
+            </div>
+        </div>
+        <div class="codeBorder fontCodeStyle">
+            <pre class="codeBorder">
         changeArr() {<br/>
           //问题:点击不更新视图<br/>
           outData.arr[1] = "ddd";<br/>
@@ -72,75 +75,66 @@
           // Vue.set(outData.arr, 1, "eee");<br/>
         },
       </pre>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import hljs from "highlight.js";
-import "highlight.js/styles/tomorrow-night-eighties.css";
-import Vue from "vue";
-
-const highlightCode = () => {
-  const block = document.querySelectorAll("pre");
-  hljs.configure({ useBR: true });
-  block.forEach((el) => {
-    hljs.highlightBlock(el);
-  });
-};
+import { highlightCode } from '../../assets/js/common';
+import Vue from 'vue';
 
 let outData = {
-  count: 1,
-  arr: ["aaa", "bbb", "ccc"],
+    count: 1,
+    arr: ['aaa', 'bbb', 'ccc'],
 };
 
 export default {
-  data() {
-    return outData;
-  },
-  mounted() {
-    highlightCode();
-  },
-
-  created() {},
-  methods: {
-    changeCount() {
-      // 方法一：利用Vue.set
-      Vue.set(outData, "count", this.count + 1);
-      // 方法二：利用this
-      // this.count++;
-      // 方法三： 直接操作外部数据(原生方法)
-      // outData.count++;
+    data() {
+        return outData;
+    },
+    mounted() {
+        highlightCode();
     },
 
-    changeArr() {
-      outData.arr[1] = "ddd";
-      // Vue.set(outData.arr, 1, "eee");
+    created() {},
+    methods: {
+        changeCount() {
+            // 方法一：利用Vue.set
+            Vue.set(outData, 'count', this.count + 1);
+            // 方法二：利用this
+            // this.count++;
+            // 方法三： 直接操作外部数据(原生方法)
+            // outData.count++;
+        },
+
+        changeArr() {
+            outData.arr[1] = 'ddd';
+            // Vue.set(outData.arr, 1, "eee");
+        },
     },
-  },
 };
 </script>
 
 <style scoped>
 .fontSize {
-  font-size: 25px;
+    font-size: 25px;
 }
 
 .codeBorder {
-  width: 80%;
-  border-radius: 10px;
+    width: 80%;
+    border-radius: 10px;
 }
 
 .fontCodeStyle {
-  line-height: 24px;
+    line-height: 24px;
 }
 
 .labelMarginLeft {
-  margin-left: 50px;
+    margin-left: 50px;
 }
 
 .vueDirective {
-  padding: 50px;
-  background-color: #ecfafe;
+    padding: 50px;
+    background-color: #ecfafe;
 }
 </style>
--- src/views/vueInterview/vueDirective.vue
+++ src/views/vueInterview/vueDirective.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="vueDirective">
-    <div class="fontStyle">
-      <p class="fontSize">1.什么是全局API？</p>
-      <p class="fontIndent">
-        全局API并不在构造器里，而是先声明全局变量或者直接在Vue上定义一些新功能，Vue内置了一些全局API，说的简单些就是，在构造器外部用Vue提供给我们的API函数来定义新的功能。
-      </p>
-      <p class="fontSize">2.自定义指令</p>
-      <p>
-        利用Vue.directive()方法，自定义vue指令，例如下面示例，在data中给定字体color，然后利用自定义属性进行改变
-        。效果图如下：
-      </p>
-      <p v-gtColor="colorValue">{{ number }}</p>
-      <p>代码如下：</p>
-    </div>
-    <div class="codeBorder fontCodeStyle">
-      <pre class="codeBorder">
+    <div class="vueDirective">
+        <div class="fontStyle">
+            <p class="fontSize">1.什么是全局API？</p>
+            <p class="fontIndent">
+                全局API并不在构造器里，而是先声明全局变量或者直接在Vue上定义一些新功能，Vue内置了一些全局API，说的简单些就是，在构造器外部用Vue提供给我们的API函数来定义新的功能。
+            </p>
+            <p class="fontSize">2.自定义指令</p>
+            <p>
+                利用Vue.directive()方法，自定义vue指令，例如下面示例，在data中给定字体color，然后利用自定义属性进行改变
+                。效果图如下：
+            </p>
+            <p v-gtColor="colorValue">{{ number }}</p>
+            <p>代码如下：</p>
+        </div>
+        <div class="codeBorder fontCodeStyle">
+            <pre class="codeBorder">
         Vue.directive("gtColor", function(el, binding, vnode){<br/>
           console.log(el, binding, vnode);<br/>
           el.style = "color: " + binding.value;<br/>
@@ -27,41 +27,41 @@
           }<br/>
         }
       </pre>
-    </div>
-    <div class="fontStyle">
-      <p>自定义指令中传递的三个参数：</p>
-      <div class="labelMarginLeft">
-        <ol type="a">
-          <li>el：指令所绑定的元素，可以用来直接操作DOM</li>
-          <li>
-            binging：一个对象，包含指令很多信息（value、name、expression）
-          </li>
-          <li>vnode：Vue编译生成的虚拟DOM</li>
-        </ol>
-      </div>
-    </div>
-    <div class="fontStyle">
-      <p class="fontSize">3.自定义指令生命周期</p>
-      <p>
-        自定义指令生命周期包括bind、inserted、update、componentUpdated、unbind，详解如下：
-      </p>
-      <div class="labelMarginLeft">
-        <ol type="a">
-          <li>
-            bind：只调用一次，指令第一次绑定到元素时调用，用这个钩子函数可以定义一个绑定时执行一次的初始化动作；
-          </li>
-          <li>inserted：被绑定元素插入到父节点时调用；</li>
-          <li>
-            update：被绑定于元素所在的模板更新时调用，而无论绑定值是否变化。通过比较更新前后的绑定值，可以忽略不必要的模板更新。
-          </li>
-          <li>componentUpdated：被绑定元素所在模板完成一次更新周期时调用。</li>
-          <li>unbind：只调用一次，指令与元素解绑时调用。</li>
-        </ol>
-      </div>
-      <p>代码如下：</p>
-    </div>
-    <div class="codeBorder fontCodeStyle">
-      <pre class="codeBorder">
+        </div>
+        <div class="fontStyle">
+            <p>自定义指令中传递的三个参数：</p>
+            <div class="labelMarginLeft">
+                <ol type="a">
+                    <li>el：指令所绑定的元素，可以用来直接操作DOM</li>
+                    <li>
+                        binging：一个对象，包含指令很多信息（value、name、expression）
+                    </li>
+                    <li>vnode：Vue编译生成的虚拟DOM</li>
+                </ol>
+            </div>
+        </div>
+        <div class="fontStyle">
+            <p class="fontSize">3.自定义指令生命周期</p>
+            <p>
+                自定义指令生命周期包括bind、inserted、update、componentUpdated、unbind，详解如下：
+            </p>
+            <div class="labelMarginLeft">
+                <ol type="a">
+                    <li>
+                        bind：只调用一次，指令第一次绑定到元素时调用，用这个钩子函数可以定义一个绑定时执行一次的初始化动作；
+                    </li>
+                    <li>inserted：被绑定元素插入到父节点时调用；</li>
+                    <li>
+                        update：被绑定于元素所在的模板更新时调用，而无论绑定值是否变化。通过比较更新前后的绑定值，可以忽略不必要的模板更新。
+                    </li>
+                    <li>componentUpdated：被绑定元素所在模板完成一次更新周期时调用。</li>
+                    <li>unbind：只调用一次，指令与元素解绑时调用。</li>
+                </ol>
+            </div>
+            <p>代码如下：</p>
+        </div>
+        <div class="codeBorder fontCodeStyle">
+            <pre class="codeBorder">
         Vue.directive("gtColor", {<br/>
           bind: function(el, binding){<br/>
             el.style = "color: " + binding.value;<br/>
@@ -80,53 +80,45 @@
           }<br/>
         })<br/>
       </pre>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import hljs from "highlight.js";
-import "highlight.js/styles/tomorrow-night-eighties.css";
+import { highlightCode } from '../../assets/js/common';
 
-const highlightCode = () => {
-  const block = document.querySelectorAll("pre");
-  hljs.configure({ useBR: true });
-  block.forEach((el) => {
-    hljs.highlightBlock(el);
-  });
-};
 export default {
-  data() {
-    return {
-      colorValue: "green",
-      number: 999,
-    };
-  },
-  mounted() {
-    highlightCode();
-  },
+    data() {
+        return {
+            colorValue: 'green',
+            number: 999,
+        };
+    },
+    mounted() {
+        highlightCode();
+    },
 };
 </script>
 
 <style scoped>
 .vueDirective {
-  padding: 50px;
-  background-color: #ecfafe;
+    padding: 50px;
+    background-color: #ecfafe;
 }
 
 .fontSize {
-  font-size: 25px;
+    font-size: 25px;
 }
 
 .codeBorder {
-  border-radius: 10px;
+    border-radius: 10px;
 }
 
 .fontCodeStyle {
-  line-height: 24px;
+    line-height: 24px;
 }
 
 .labelMarginLeft {
-  margin-left: 30px;
+    margin-left: 30px;
 }
 </style>
